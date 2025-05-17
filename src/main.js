2b1df676dd25f3b56fe8296fb4e1d621958d5b7a
anseka <nozomunks@mail.ru>
2025-05-17 15:44:28 +0300

    Добавлен функционал сохранения маршрутов(main.js) и отображения сохранённого списка

--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@
 //Пример импорта js-файла
 import setupCounter from './js/counter.js';
 
-
 //Вызываем свои функции в проекте
 document.addEventListener('DOMContentLoaded', () => {
-  setupCounter() //вызов функции
+	setupCounter(); //вызов функции
 });
+
+//Функция сохранения маршрута
+export function saveRouteToLocalStorage(newRoute) {
+	const raw = localStorage.getItem('savedRoutes');
+	const routes = raw ? JSON.parse(raw) : [];
+
+	routes.push(newRoute);
+	localStorage.setItem('savedRoutes', JSON.stringify(routes));
+}
